6a38c5c4b798cd9701677ef1fc3101005f0329e4
PoetaKodu <rfpawelsyska@gmail.com>
2019-11-27 00:27:05 +0100

    Implemented basic parenting behaviour for Element class.

--- include/Rexrn/GenericUI/Element.hpp
+++ include/Rexrn/GenericUI/Element.hpp
@@ -15,6 +15,34 @@ class Element
 	: public Node
 {
 public:
+	/// <summary>Appends a child.</summary>
+	/// <param name="node_">The child node.</param>
+	void appendChild( std::unique_ptr<Node> node_ );
+
+	/// <summary>Removes every child.</summary>
+	/// <param name="releaseMemory_">Whether to release memory used by the children container.</param>
+	void clearChildren(bool releaseMemory_ = true)
+	{
+		_children.clear();
+		if (releaseMemory_)
+			_children.shrink_to_fit();
+	}
+
+	/// <summary>Releases children.</summary>
+	auto releaseChildren()
+	{
+		auto ret = std::move(_children);
+		this->clearChildren(); // TODO: check if this is necessary.
+		return ret;
+	}
+
+	/// <summary>Returns the children container (by cref).</summary>
+	auto const& getChildren() const
+	{
+		return _children;
+	}
+protected:
+	std::vector< std::shared_ptr<Node> > _children;
 };
 
 }
